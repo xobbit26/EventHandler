54a831cc736ff18c6d8a68232d22dc4172d79d00
Stanislav Naravetskas <stanislav.naravetskas@arcadia.spb.ru>
2019-10-03 19:33:59 +0300

    added EntityFramework and base structure to DAL project

---
+++ EventHandler/EventHandler.DAL/Entities/Event.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventHandler.DAL.Entities
+{
+    class Event
+    {
+        public long Id { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ EventHandler/EventHandler.DAL/EventHandlerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EventHandler.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventHandler.DAL
+{
+    class EventHandlerDbContext : DbContext
+    {
+        DbSet<Event> Events { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql(@"Host=localhost;Port=5433;Database=EventHandler;Username=postgres;Password=admin");
+            }
+        }
+    }
+}
