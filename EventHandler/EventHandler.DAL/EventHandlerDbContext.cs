abf345d48288c75603a9f1edf4f66852b791f272
Stanislav Naravetskas <stanislav.naravetskas@arcadia.spb.ru>
2019-10-08 21:28:56 +0300

    changed a configuration way. Now db context is configured in web project in the startup class

--- EventHandler/EventHandler.DAL/EventHandlerDbContext.cs
+++ EventHandler/EventHandler.DAL/EventHandlerDbContext.cs
@@ -4,24 +4,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EventHandler.DAL
 {
-    class EventHandlerDbContext : DbContext
+    public class EventHandlerDbContext : DbContext
     {
-        private readonly Configuration _configuration;
-
-        public EventHandlerDbContext()
+        public EventHandlerDbContext(DbContextOptions options)
+            : base(options)
         {
-            _configuration = new Configuration();
         }
 
         DbSet<Event> Events { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseNpgsql(_configuration.SqlConnectionString);
-            }
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- EventHandler/EventHandler.Web/Startup.cs
+++ EventHandler/EventHandler.Web/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EventHandler.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,22 +16,28 @@ namespace EventHandler.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Configuring db context
+            services.AddDbContext<EventHandlerDbContext>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("DataConnection"),
+                    assembly => assembly.MigrationsAssembly(typeof(EventHandlerDbContext).Assembly.FullName));
+            });
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        {            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
